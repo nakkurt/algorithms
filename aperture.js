c7a8bb9bff71887f51032b2b03f917a27b21480e
nakkurt <andrenakkurt@gmail.com>
2018-03-13 23:40:33 -0700

    aperture

---
+++ aperture.js
@@ -0,0 +1,40 @@
+/*
+Write a function called aperture which accepts a number and an array and returns
+ a new array. The new array should be composed of subarrays the size of the number
+ with consecutive elements. If the number passed to this function is greater than 
+the length of the array or less than 1, an empty array is returned.
+
+Examples:
+
+aperture(0, [1, 2, 3, 4, 5]); // []
+aperture(1, [1, 2, 3, 4, 5]); // [[1], [2], [3], [4], [5]]
+aperture(2, [1, 2, 3, 4, 5]); // [[1, 2], [2, 3], [3, 4], [4, 5]]
+aperture(3, [1, 2, 3, 4, 5]); // [[1, 2, 3], [2, 3, 4], [3, 4, 5]]
+aperture(4, [1, 2, 3, 4, 5]); // [[1, 2, 3, 4], [2, 3, 4, 5]]
+aperture(5, [1, 2, 3, 4, 5]); // [[1, 2, 3, 4, 5]]
+aperture(6, [1, 2, 3, 4, 5]); // []
+
+*/
+
+function aperture (n,arr) {
+  //create empty result array
+  var result = [];
+  // return empty array if n 0 or n > arr.length
+  if(n > arr.length || n === 0) {
+    return result;
+  }
+  //create subarrays with number of elements matching n
+  //define subarray count
+  subArrCount = arr.length-n+1 
+  //create subarrays wih values from array
+  var i = 0;
+  while(i < subArrCount) {
+  
+  result.push(arr.slice(i,n+i));
+  //push subarrays into result array
+  i++;
+  }
+
+  // return result array
+  return result;
+}
