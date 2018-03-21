6ca10f5c2d85d7621c5f5833a00206dfd855ef96
nakkurt <andrenakkurt@gmail.com>
2018-03-20 23:59:46 -0700

    omitBy

---
+++ omitBy.js
@@ -0,0 +1,31 @@
+/*
+Write a function called omitBy which accepts an object and a callback function. 
+This function should return an object with all the keys and values for which the|
+ values return falsey values when passed into the callback function.
+
+Examples:
+
+function isNumber(val) {
+  return Number.isFinite(val);
+}
+
+let isString(val) {
+  return typeof val === "string";
+}
+
+let object = { 'a': 1, 'b': '2', 'c': 3 };
+omitBy(object, isNumber) // { 'b': '2' }
+omitBy(object, isString) // { 'a': 1, 'c': 3 }
+
+*/
+
+function omitBy(obj, cb){
+  let newObj = {};
+  for(let key in obj){
+    if(!cb(obj[key])){
+      newObj[key] = obj[key]
+    }
+  }
+  return newObj;
+}
+
