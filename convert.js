8ac3637da4bef5804b18342e4d85218122ec1445
nakkurt <andrenakkurt@gmail.com>
2018-03-28 22:53:32 -0700

    convert

---
+++ convert.js
@@ -0,0 +1,35 @@
+// Convert
+// Given a string of characters a-z (all lowercase), convert it to its char 
+// count representation: 
+
+// Ex 1: 
+// input: 'z'
+// output: '1z'
+
+// Ex 2:
+// input: 'banana'
+// output: '1b3a2n'
+
+// Please note that the order does not matter. Either of the following examples 
+// are considered correct: 
+
+// Ex 3: 
+// input: 'aabb'
+// output: '2a2b'
+
+// Ex 4: 
+// input: 'aabb'
+// output: '2b2a'
+
+function convert(str) {
+  let result = '';
+  let charCount = {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0};
+  for(let i = 0; i < str.length; i++) {
+	charCount[str[i]]++;
+  }
+  for(let key in charCount) {
+    if(charCount[key] === 0) {delete charCount[key]}
+    else {result += charCount[key].toString() + key}
+  }
+  return result; 
+}
