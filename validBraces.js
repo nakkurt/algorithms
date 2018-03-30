722d4767d1672e7b315f463f9800a61505ba489b
nakkurt <andrenakkurt@gmail.com>
2018-03-29 23:54:49 -0700

    validBraces_formatting

--- validBraces.js
+++ validBraces.js
@@ -1,6 +1,10 @@
-/*Write a function called validBraces that takes a string of braces, and determines if the order of the braces is valid. validBraces should return true if the string is valid, and false if it's invalid.
+/*Write a function called validBraces that takes a string of braces, 
+and determines if the order of the braces is valid. validBraces should
+ return true if the string is valid, and false if it's invalid.
 
-This problem is similar to validParenthesis, but introduces four new characters. Open and closed brackets, and open and closed curly braces. All input strings will be nonempty, and will only consist of:
+This problem is similar to validParenthesis, but introduces four new 
+characters. Open and closed brackets, and open and closed curly braces.
+ All input strings will be nonempty, and will only consist of:
 
 - open parentheses '('  
 - closed parentheses ')' 
