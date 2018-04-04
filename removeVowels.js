e51891e6562ab11c683010c846289e707dbc7a88
nakkurt <andrenakkurt@gmail.com>
2018-04-04 15:53:35 -0700

    removeVowels

---
+++ removeVowels.js
@@ -0,0 +1,15 @@
+// Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
+
+// Examples:
+
+// removeVowels('hELLO') // 'hll'
+// removeVowels('ZZZZZZ') // 'zzzzzz'
+
+// Try to solve this using filter.
+
+function removeVowels(str){
+  let strArr = str.toLowerCase().split('');
+  let vowels = 'aeiou'
+  let vowelsRemoved = strArr.filter(char => !vowels.includes(char));
+  return vowelsRemoved.join('')
+}
